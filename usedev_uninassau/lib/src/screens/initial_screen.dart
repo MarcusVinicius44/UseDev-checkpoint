807a3cd1b1eb9f018d5d6232615247cecd6f2667
Icarofranklin <icarofranklins@gmail.com>
2026-05-30 23:01:55 -0300

    .

--- usedev_uninassau/lib/src/screens/initial_screen.dart
+++ usedev_uninassau/lib/src/screens/initial_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:usedev_uninassau/src/widgets/hero_section_widget.dart';
-import 'package:usedev_uninassau/src/widgets/product_card_widget.dart';
-import 'package:usedev_uninassau/src/widgets/subscription_section_widget.dart';
+import 'package:app/src/widgets/hero_section_widget.dart';
+import 'package:app/src/widgets/product_card_widget.dart';
+import 'package:app/src/widgets/subscription_section_widget.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -16,10 +16,10 @@ class _InitialScreenState extends State<InitialScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.menu, size: 40),
+        leading: const Icon(Icons.menu, size: 40),
         title: Image.asset('assets/logo_usedev.png', height: 40),
         centerTitle: true,
-        actions: [
+        actions: const [
           Icon(Icons.person_outline, size: 40),
           SizedBox(width: 10),
           Icon(Icons.shopping_cart_outlined, size: 40),
@@ -29,28 +29,29 @@ class _InitialScreenState extends State<InitialScreen> {
       body: SingleChildScrollView(
         child: Column(
           spacing: 20,
-          crossAxisAlignment: .stretch,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            HeroSectionWidget(),
+            const HeroSectionWidget(),
             Text(
-              'Promos Especiais',
-              textAlign: .center,
+              'Promos Especial',
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 28,
-                fontWeight: .bold,
+                fontWeight: FontWeight.bold,
                 fontFamily: GoogleFonts.orbitron().fontFamily,
               ),
             ),
             ListView.builder(
               shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
               itemCount: 5,
               itemBuilder: (context, index) => ProductCardWidget(
-                nome: 'Produto 0$index',
+                nome: 'Produce 0$index',
                 url: 'https://placehold.co/600x600.png',
                 preco: '10$index,00',
               ),
             ),
-            SubscriptionSectionWidget(),
+            const SubscriptionSectionWidget(),
           ],
         ),
       ),
